557663ce015193b8aa11c5d3262172b154f98764
truongson00 <soi30082000@gmail.com>
2021-11-28 16:06:01 +0700

    push from terminal

---
+++ test.java
@@ -0,0 +1,8 @@
+package thi;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("commit to github");
+        System.out.println("commit 2 to git hub");
+    }
+}
